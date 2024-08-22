b0ecf21be9a4c981b91a4f6316def104b52b1275
rohan-sav <rohsavofficial@gmail.com>
2024-08-22 12:24:59 +1200

    music v1

--- New Platfromer/Menu/main_menu.gd
+++ New Platfromer/Menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +13,11 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 		get_tree().change_scene_to_file("res://levels/level_00.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass
